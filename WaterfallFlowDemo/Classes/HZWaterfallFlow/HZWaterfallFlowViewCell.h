494d145806ffa96c4a0663ec3d1f06e722d56319
e29hz <ehongzhen@gmail.com>
2016-08-30 15:31:12 +0800

    Revert "取消搜索,改为历史记录"
    This reverts commit a5254131f50851ad3418e2d309164c76fcf2d334.

---
+++ WaterfallFlowDemo/Classes/HZWaterfallFlow/HZWaterfallFlowViewCell.h
@@ -0,0 +1,16 @@
+//
+//  HZWaterfallFlowViewCell.h
+//  WaterfallFlowDemo
+//
+//  Created by 鄂鸿桢 on 16/2/24.
+//  Copyright © 2016年 e29hz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HZWaterfallFlowViewCell : UIView
+
+@property (nonatomic, copy) NSString *identifier;
+
+
+@end
